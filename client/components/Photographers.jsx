8208c7ff7d60cf1c7f7c9c89c7d46e620580c24e
Jatin-Puri <jatinpuri11@gmail.com>
2021-04-26 15:08:17 +1200

    fix typo in photographers

--- client/components/Photographers.jsx
+++ client/components/Photographers.jsx
@@ -3,8 +3,10 @@ import { HashRouter as Router, Link } from 'react-router-dom'
 
 import { getPhotographers } from '../api'
 
-function Photographers () {
+function Photographers (props) {
     const [photographers, setPhotographers] = useState([])
+
+    console.log(props.match.params)
 
     useEffect(() => {
         getPhotographers()
@@ -18,8 +20,15 @@ function Photographers () {
           })  
       }, [])
 
+// const currentPhotographer = photographers.find((props) => {
+//     return props.match.params
+// })
+
       return (
         <div>
+
+        {/* //if not currentPhotographer show loading else show photgrapher details */}
+
         {photographers.map(({name, location, image}) => 
           <>
             <h1 className='title'>{name}</h1> 
