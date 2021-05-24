f99eade02893de45dc51a92faad5fac78d0fb06c
Jatin-Puri <jatinpuri11@gmail.com>
2021-05-24 16:04:11 +1200

    change port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const server = require('./server')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 server.listen(port, function () {
   // eslint-disable-next-line no-console
