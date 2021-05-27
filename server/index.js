c59093222656d75691ef499399e59465e0c8629a
Jatin-Puri <jatinpuri11@gmail.com>
2021-05-27 22:23:27 +1200

    heroku deploy

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
-require('dotenv').config()
-
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  const envConfig = require('dotenv').config()
+  if (envConfig.error) throw envConfig.error
+}
 const server = require('./server')
 
 const port = process.env.PORT || 3000
