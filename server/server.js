1c525a5c220aa290d5a9b7066e76d70f3924cb87
Jatin-Puri <jatinpuri11@gmail.com>
2021-05-31 11:05:36 +1200

    clean server file

--- server/server.js
+++ server/server.js
@@ -10,13 +10,6 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 server.use(cors('*'))
 
-if (process.env.NODE_ENV === 'production') {
-  server.use(express.static('client/build'))
-}
-server.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-})
-
 server.use('/api/v1/photographers', photographers)
 server.use('/api/v1/auth', passportRoute)
 
